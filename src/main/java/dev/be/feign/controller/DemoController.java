4c8d17c03c4a178d6d8a240c3d5571ca3e41572b
jaemin <jjm7655@gmail.com>
2024-09-12 19:33:08 +0900

    feat: feign client Get으로 target server의 데이터 가져오기.
    DemoController get() -> feign client get() -> response

---
+++ src/main/java/dev/be/feign/controller/DemoController.java
@@ -0,0 +1,18 @@
+package dev.be.feign.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.be.feign.service.DemoService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class DemoController {
+	private final DemoService demoService;
+
+	@GetMapping("/get")
+	public String getController() {
+		return demoService.get();
+	}
+}
